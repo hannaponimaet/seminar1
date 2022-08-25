332a0173c7c31878eed2c8026e6ddfd98e9d742a
Hanna Pon <hannainleto@mail.ru>
2022-08-25 19:22:00 +0300

    Задача 2

---
+++ 002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine("min = "+b);
+}
+else if (b>a)
+{
+    Console.WriteLine("max = "+b);
+}
