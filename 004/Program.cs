98ae1239492f739d95831ca63ff86e25116c20dc
Hanna Pon <hannainleto@mail.ru>
2022-08-25 19:47:00 +0300

    Задача 4

---
+++ 004/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число а: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b && a > c)
+{
+    Console.WriteLine("Nubmer Max = " + a); 
+}
+else if (b > c)
+{
+    Console.WriteLine("Number Max = " + b);
+}
+else
+{
+    Console.WriteLine("Number Max = " + c);
+}
+Console.ReadLine();
